18d217f3691534b3ed05ab7d850ea5cdf61e3747
Mauricio Durán Torres <mauriciodurant@gmail.com>
2017-08-19 18:01:06 +0000

    se agrega validación al controller de Home para ver el perfil de usuario y redireccionar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   resources :questions
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
